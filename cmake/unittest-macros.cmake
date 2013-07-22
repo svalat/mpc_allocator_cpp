aed706bf11a9c1076ee1981d4c817fe2a104b4e4
Sebastien Valat <sebastien.valat.dev@orange.fr>
2013-07-22 21:29:51 +0200

    * import test wrapper from CMR project

---
+++ cmake/unittest-macros.cmake
@@ -0,0 +1,26 @@
+######################################################
+#            PROJECT  : CartesianMeshRuntime         #
+#            VERSION  : 0.0.0                        #
+#            DATE     : 02/2013                      #
+#            AUTHOR   : Valat Sébastien              #
+#            LICENSE  : CeCILL-C                     #
+######################################################
+
+######################################################
+#For integration of tests in jenkins, but only in self-test mode
+macro (cmr_add_test test_name)
+	#steup wrapper
+	if (ENABLE_VALGRIND)
+			set(tmp_test_wrapper valgrind --xml=yes --xml-file=${CMAKE_CURRENT_BINARY_DIR}/${test_name}.memcheck.xml --tool=memcheck --leak-check=full --show-reachable=yes)
+	else(ENABLE_VALGRIND)
+			set(tmp_test_wrapper "")
+	endif(ENABLE_VALGRIND)
+
+	#setup output options
+	if (ENABLE_JUNIT_OUTPUT)
+		set(tmp_test_run_option --gtest_output=xml:${CMAKE_CURRENT_BINARY_DIR}/${test_name}.gtest.xml)
+	endif (ENABLE_JUNIT_OUTPUT)
+
+	#gen text command and register
+	add_test(${test_name} ${tmp_test_wrapper} ${CMAKE_CURRENT_BINARY_DIR}/${test_name} ${tmp_test_run_option})
+endmacro (cmr_add_test)
