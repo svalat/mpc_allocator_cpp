a2190aade546fde844a112b9bd14010de2dd0597
Sebastien Valat <sebastien.valat.dev@orange.fr>
2013-07-29 21:36:06 +0200

    * Permit to disable unit tests via --disable-tests of -DDISABLE_TESTS

--- cmake/check-deps.cmake
+++ cmake/check-deps.cmake
@@ -9,8 +9,10 @@
 ######################################################
 #For allocator
 find_package(Threads REQUIRED)
-#For tests
-find_package(OpenMP  REQUIRED)
-find_package(GTest   REQUIRED)
-find_package(GMock   REQUIRED)
 find_package(OpenPA  REQUIRED)
+#For tests
+if (NOT DISABLE_TESTS)
+	find_package(OpenMP  REQUIRED)
+	find_package(GTest   REQUIRED)
+	find_package(GMock   REQUIRED)
+endif (NOT DISABLE_TESTS)
