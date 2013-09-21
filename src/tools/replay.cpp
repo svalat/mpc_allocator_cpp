3545d278d1f4147bade26c31bfd72ef981757cbb
Sebastien Valat <sebastien.valat.dev@orange.fr>
2013-09-21 20:30:22 +0200

    * minor fix

--- src/tools/replay.cpp
+++ src/tools/replay.cpp
@@ -7,7 +7,8 @@
 #include "TraceReader.h"
 #include "CollisionChecker.h"
 
-#define printf(...) do{} while(0)
+//make it silent
+#define trace_printf(...) do{} while(0)
 
 /**********************  USING  *********************/
 using namespace std;
@@ -39,42 +40,42 @@ int main(int argc,char ** argv)
 		switch(entry.call.type)
 		{
 			case TRACE_MALLOC:
-				printf("%p = malloc(%lu) //timestamp = %lu, thread = %d\n",
+				trace_printf("%p = malloc(%lu) //timestamp = %lu, thread = %d\n",
 					   (void*)entry.result,entry.call.infos.malloc.size,entry.call.timestamp,(int)entry.call.threadId);
 				checkerTrace.registerSegment((void*)entry.result,entry.call.infos.malloc.size);
 				ptr = alloc.malloc(entry.call.infos.malloc.size);
-				printf("          => %p , %lu\n",ptr,alloc.getInnerSize(ptr));
+				trace_printf("          => %p , %lu\n",ptr,alloc.getInnerSize(ptr));
 				ptrs[entry.result] = ptr;
 				checker.registerSegment(ptr,alloc.getInnerSize(ptr));
 				memset(ptr,0,entry.call.infos.malloc.size);
 				break;
 			case TRACE_FREE:
 				ptr = ptrs[entry.call.infos.free.ptr];
-				printf("free(%p) //timestamp = %lu, thread = %d, realptr = %p\n",
+				trace_printf("free(%p) //timestamp = %lu, thread = %d, realptr = %p\n",
 					   (void*)entry.call.infos.free.ptr,entry.call.timestamp,(int)entry.call.threadId,ptr);
 				checkerTrace.unregisterSegment((void*)entry.call.infos.free.ptr,1);
 				checker.unregisterSegment(ptr,alloc.getInnerSize(ptr));
 				alloc.free(ptr);
 				break;
 			case TRACE_CALLOC:
-				printf("%p = calloc(%lu,%lu) //timestamp = %lu, thread = %d\n",
+				trace_printf("%p = calloc(%lu,%lu) //timestamp = %lu, thread = %d\n",
 					   (void*)entry.result,entry.call.infos.calloc.nmemb,entry.call.infos.calloc.size,entry.call.timestamp,(int)entry.call.threadId);
 				checkerTrace.registerSegment((void*)entry.result,entry.call.infos.calloc.nmemb*entry.call.infos.calloc.size);
 				ptr = alloc.calloc(entry.call.infos.calloc.nmemb,entry.call.infos.calloc.size);
-				printf("          => %p , %lu\n",ptr,alloc.getInnerSize(ptr));
+				trace_printf("          => %p , %lu\n",ptr,alloc.getInnerSize(ptr));
 				ptrs[entry.result] = ptr;
 				checker.registerSegment(ptr,alloc.getInnerSize(ptr));
 				checkIsZero(ptr,entry.call.infos.calloc.nmemb*entry.call.infos.calloc.size);
 				break;
 			case TRACE_REALLOC:
 				ptr = ptrs[entry.call.infos.realloc.oldPtr];
-				printf("%p = relloc(%p,%lu) //timestamp = %lu, thread = %d, realptr = %p\n",
+				trace_printf("%p = relloc(%p,%lu) //timestamp = %lu, thread = %d, realptr = %p\n",
 					   (void*)entry.result,(void*)entry.call.infos.realloc.oldPtr,entry.call.infos.realloc.newSize,entry.call.timestamp,(int)entry.call.threadId,ptr);
 				checkerTrace.unregisterSegment((void*)entry.call.infos.realloc.oldPtr,1);
 				checkerTrace.registerSegment((void*)entry.result,entry.call.infos.realloc.newSize);
 				checker.unregisterSegment(ptr,alloc.getInnerSize(ptr));
 				ptr = alloc.realloc(ptr,entry.call.infos.realloc.newSize);
-				printf("          => %p , %lu\n",ptr,alloc.getInnerSize(ptr));
+				trace_printf("          => %p , %lu\n",ptr,alloc.getInnerSize(ptr));
 				ptrs[entry.result] = ptr;
 				checker.registerSegment(ptr,alloc.getInnerSize(ptr));
 				memset(ptr,0,entry.call.infos.realloc.newSize);
@@ -82,18 +83,18 @@ int main(int argc,char ** argv)
 			case TRACE_MEMALIGN:
 			case TRACE_ALIGN_ALLOC:
 			case TRACE_POSIX_MEMALIGN:
-				printf("%p = memalign(%lu,%lu) //timestamp = %lu, thread = %d\n",
+				trace_printf("%p = memalign(%lu,%lu) //timestamp = %lu, thread = %d\n",
 					   (void*)entry.result,entry.call.infos.memalign.align,entry.call.infos.memalign.size,entry.call.timestamp,(int)entry.call.threadId);
 				checkerTrace.registerSegment((void*)entry.result,entry.call.infos.memalign.size);
 				ptr = alloc.memalign(entry.call.infos.memalign.align,entry.call.infos.memalign.size);
-				printf("          => %p , %lu\n",ptr,alloc.getInnerSize(ptr));
+				trace_printf("          => %p , %lu\n",ptr,alloc.getInnerSize(ptr));
 				ptrs[entry.result] = ptr;
 				checker.registerSegment(ptr,alloc.getInnerSize(ptr));
 				memset(ptr,0,entry.call.infos.memalign.size);
 				break;
 			case TRACE_PVALLOC:
 			case TRACE_VALLOC:
-				printf("pvalloc/valloc\n");
+				trace_printf("pvalloc/valloc\n");
 				assert(false);
 				break;
 			default:
